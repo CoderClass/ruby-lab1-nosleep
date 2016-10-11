05c1e7fa8ba3599c753e4b3fedd8f3ba7d04a89d
dinhngochien <hiendinhngoc@yahoo.com.vn>
2016-10-11 23:22:27 +0700

    Complete bonus mistone one

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-  Photo.create(
-  {
-    username: Faker::Name.name,
-    caption: Faker::Lorem.sentence,
-    likes_count: Faker::Number.number(5),
-    url: Faker::Avatar.image
-  })
+# 5.times do
+#   Photo.create(
+#   {
+#     username: Faker::Name.name,
+#     caption: Faker::Lorem.sentence,
+#     likes_count: Faker::Number.number(5),
+#     url: Faker::Avatar.image
+#   })
+# end
+
+Photo.all.each do |photo|
+  rand(5).times do 
+  	photo.comments.create! username: Faker::Internet.user_name, body: Faker::Hispter.sentence
+  end
 end
